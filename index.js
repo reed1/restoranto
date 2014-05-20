7d658ad3c0766487b27a6f4e43c2d532fbb09f7d
Ridwan Mulyadi <ridwan.mulyadi@live.com>
2014-05-20 17:33:42 +0700

    routing fix

--- index.js
+++ index.js
@@ -8,4 +8,12 @@ var app = express();
 require('./lib/petakumpet/express')(app);
 require('./lib/petakumpet/routing')(app);
 
-app.listen(config.web.port);
+app.listen(config.web.port, function(err) {
+
+  if(err) {
+    console.log(err);
+  } else {
+    console.log('App listening on port: ', config.web.port);
+  }
+
+});
